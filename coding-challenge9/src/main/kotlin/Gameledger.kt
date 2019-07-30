620a49a29f4750d9dd550f55912e243dd4e7758f
bromley.jones <bromley.jones@johnlewis.com>
2019-07-30 11:19:23 +0100

    removed redundant code

--- coding-challenge9/src/main/kotlin/Gameledger.kt
+++ coding-challenge9/src/main/kotlin/Gameledger.kt
@@ -1,10 +1,5 @@
 package codingchallenge9
 
-import java.lang.IllegalArgumentException
-
-const val ERROR_INVALID_TRANSACTION_TYPE = "Invalid Transaction Type for passed parameters"
-
-
 class Gameledger {
     private val transactionHistory: MutableList<ITransaction> = mutableListOf()
 
@@ -55,21 +50,4 @@ class Gameledger {
         return transactionHistory.last()
     }
 
-    /*
-    // Pay Rent, Fee or Purchase a location
-    fun addTransaction (transactionType : TransactionType,
-                        fromAccountHolder : IAccountHolder,
-                        toAccountHolder : IAccountHolder,
-                        location : Location ) : ITransaction {
-
-        when (transactionType){
-            TransactionType.RENTPAYMENT -> transactionHistory.add(RentPaymentTxn(fromAccountHolder,toAccountHolder,location))
-            TransactionType.BANKPAYMENT -> transactionHistory.add(BankFeeTxn(fromAccountHolder,toAccountHolder,location))
-            TransactionType.LOCATIONPURCHASE -> transactionHistory.add(PurchaseLocationTxn(fromAccountHolder,toAccountHolder,location))
-            else -> throw IllegalArgumentException (ERROR_INVALID_TRANSACTION_TYPE)
-        }
-        return transactionHistory.last()
-    }
-
-     */
 }
