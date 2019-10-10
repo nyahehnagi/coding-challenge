12992881fcedb4a8c79cbbd51bf05572eca5f3dc
Bromley Jones <bromley.jones@johnlewis.co.uk>
2019-10-10 09:44:44 +0100

    fixed tests

--- coding-challenge13/src/test/kotlin/ShopDistancesTest.kt
+++ coding-challenge13/src/test/kotlin/ShopDistancesTest.kt
@@ -48,10 +48,10 @@ class ShopDistancesTest {
     fun `Should test that a list of stores is created from a comma delimited string and with missing data for geolocation`() {
         val storeString:  String = "Victoria,SW1E 5NN,1.2345,2.3456,Crap Store, CR1,,,West Norwood,SE27 0SH,3.4567,4.5678"
         val storeList = deserialiseStoreString(storeString)
-        assertThat(storeList[0].storeGeoLocation.longitude, equalTo(2.3456))
+        assertThat(storeList[0].storeGeoLocation.longitude, equalTo(1.2345))
         assertThat(storeList[0].storePostCode, equalTo("SW1E 5NN"))
         assertThat(storeList[0].storeName, equalTo("Victoria"))
-        assertThat(storeList[1].storeGeoLocation.longitude, equalTo(4.5678))
+        assertThat(storeList[1].storeGeoLocation.longitude, equalTo(3.4567))
         assertThat(storeList[1].storeName, equalTo("West Norwood"))
         assertThat(storeList[1].storePostCode, equalTo("SE27 0SH"))
     }
@@ -86,7 +86,7 @@ class ShopDistancesTest {
         val currentStore :Store = Store("Abergavenny","Post Code",geoLocation3)
         val storeList : List<Store> = listOf(Store("Oxford Street","Post Code",geoLocation2), Store("171 Victoria","Post Code",geoLocation1),Store("Abingdon","Post Code",geoLocation4))
 
-        val closestStore = currentStore.findClosestShop(storeList)
+        val closestStore = currentStore.findClosestStore(storeList)
 
 
         assertThat(closestStore.storeName, equalTo("Abingdon")) //value checked with an online calculator
